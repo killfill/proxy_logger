20022fd6d9b561599081640298e200770db4117b
KillFill <pneumann@gmail.com>
2011-07-04 16:34:04 -0400

    Start making a little order :)

---
+++ run.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var app = require('./lib/app.js')
+
+var opts = require('optimist')
+  .default('controlport', 8099)
+  .default('proxyport', 9099)
+  .default('defaultmime', 'xml')
+  .default('logconsole', 'true')
+  .default('remember', 4000)
+
+  //need to get out via a remote proxy?
+  .default('remoteproxyhost', false)
+  .default('remoteproxyport', 8080)
+
+  .default('die', true)
+
+  .usage('Usage: $0 opts')
+  .argv
+
+app.init(opts);
+app.start();
